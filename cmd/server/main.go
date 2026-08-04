4d2f4460b2a79654d035b857dc44fb5c20672e98
rubanik.roman <rubanik.roman@wb.ru>
2026-08-04 22:50:04 +0300

    create config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"net/http"
 
+	"github.com/bookshelf/monolith/internal/config"
 	"github.com/bookshelf/monolith/internal/handler"
 )
 
 func main() {
 
+	cfg := config.Load()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/health", handler.HealthHandler)
 
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":"+cfg.Port, mux)
 }
