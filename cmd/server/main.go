1e451fe760af8d8c156c3e803812031295014667
rubanik.roman <rubanik.roman@wb.ru>
2026-08-06 16:49:11 +0300

    add db connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/bookshelf/monolith/internal/config"
 	"github.com/bookshelf/monolith/internal/handler"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	cfg := config.Load()
+
+	db, err := sqlx.Connect("postgres", cfg.DatabaseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
 	r := chi.NewRouter()
 
